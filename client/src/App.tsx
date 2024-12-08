94791fdcd713bf8a1233d639f442fdd78150b0b4
Sahil Shangloo <sahilshangloo35@gmail.com>
2024-12-08 19:59:59 +0530

    Restarted Frontend properly, made the color configurations in tailwind.config.json file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,9 @@
 import "./App.css";
-import { Button } from "./components/ui/Button";
-import { PlusIcon } from "./icons/PlusIcon";
-import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
     return (
         <div>
-            <Button
-                variant="primary"
-                title="Share"
-                size="lg"
-                startIcon={<PlusIcon size={"lg"} />}
-                endIcon={<ShareIcon size="lg" />}
-            />
-
-            <Button
-                variant="secondary"
-                title="Add Content"
-                size="lg"
-                startIcon={<PlusIcon size={"lg"} />}
-                endIcon={<ShareIcon size="lg" />}
-            />
-
-            <Button
-                variant="primary"
-                title="New One"
-                size="sm"
-                startIcon={<PlusIcon size={"sm"} />}
-                endIcon={<ShareIcon size="sm" />}
-            />
-
-            <Button
-                variant="primary"
-                title="Share"
-                size="md"
-                startIcon={<PlusIcon size={"md"} />}
-                endIcon={<ShareIcon size="md" />}
-            />
+            
         </div>
     );
 }
