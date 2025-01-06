d1c73089934e0053a51107b2e056d48a902594c8
Sahil Shangloo <sahilshangloo35@gmail.com>
2025-01-06 17:16:03 +0530

    Public route updations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,40 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard } from "./pages/Dashboard";
 import LandingPage from "./pages/LandingPage";
 import { SharedContent } from "./pages/SharedContent";
 import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const isAuthenticated = () => {
+    return !!localStorage.getItem("token");
+};
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+    return isAuthenticated() ? children : <Navigate to="/signin" />;
+};
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* Public Routes */}
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/signin" element={<Signin />} />
-                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/share/:shareLink" element={<SharedContent />} />
+
+                {/* Protected Routes */}
+                <Route
+                    path="/dashboard"
+                    element={
+                        <ProtectedRoute>
+                            <Dashboard />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
         </BrowserRouter>
-    )
+    );
 }
 
 export default App;
